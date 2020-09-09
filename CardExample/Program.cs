b0a1d2eb2c811541598fc3cfe0eb076b5fb7a36d
Jupshaw97 <70708707+Jupshaw97@users.noreply.github.com>
2020-09-09 15:49:21 -0400

    adds week 1 coding challenges

---
+++ CardExample/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace CardGame
+{
+    public class CardType
+    {
+
+    }
+
+    public class Deck
+    {
+
+    }
+
+    public static void main(string[] args)
+    {
+        Console.WriteLine("Hello World!")
+    }
+}
